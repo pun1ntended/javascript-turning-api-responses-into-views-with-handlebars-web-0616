29b41458a2d210b5b6ce15119a5916c26efd7ce3
Steven McGrath <mcgrath.steven7@gmail.com>
2016-08-03 11:05:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-function getRepositories() {
-  const req = new XMLHttpRequest()
-  req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/users/pun1ntended/repos')
-  req.send()
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
 }
-function showRepositories(event, data) {
-  const repos = JSON.parse(this.responseText)
-  const src = document.getElementById("repository-template").innerHTML
-  const template = Handlebars.compile(src)
-  const repoList = template(repos)
-  document.getElementById("repositories").innerHTML = repoList
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    const template = Handlebars.compile($('#results-template').html())
+    $('#results').html(template(data))
+  }).fail(error => {
+    displayError()
+  })
 }
-document.addEventListener("DOMContentLoaded", function(event) {
-  Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
+
+function showCommits(el) {
+  const owner = el.dataset.owner
+  const repo = el.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+    const template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+function handlebarsSetup() {
+  //put any handlebars setup in here
+  Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+}
+
+$(document).ready(function (){
+  handlebarsSetup()
 });
